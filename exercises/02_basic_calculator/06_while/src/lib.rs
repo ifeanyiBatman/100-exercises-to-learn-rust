a3a417c026cae8040e56a133a196f256d3dc58c2
Ifeanyi batman <anyimifeanyi0@gmail.com>
2026-05-02 17:24:05 +0100

    Complete basic calculator exercises

--- exercises/02_basic_calculator/06_while/src/lib.rs
+++ exercises/02_basic_calculator/06_while/src/lib.rs
@@ -4,7 +4,16 @@ pub fn factorial(n: u32) -> u32 {
     // interprets as "I'll get back to this later", thus
     // suppressing type errors.
     // It panics at runtime.
-    todo!()
+     if n == 0 {
+     return 1 
+     }
+    let mut facsum : u32 = 1;
+    let mut i : u32 = n; 
+    while i >= 1 {
+        facsum *= i;
+        i -= 1;
+    }
+    return facsum
 }
 
 #[cfg(test)]
--- exercises/02_basic_calculator/07_for/src/lib.rs
+++ exercises/02_basic_calculator/07_for/src/lib.rs
@@ -1,6 +1,13 @@
 // Rewrite the factorial function using a `for` loop.
 pub fn factorial(n: u32) -> u32 {
-    todo!()
+    if n <= 0 {
+        return 1
+    }
+    let mut facsum : u32 = 1;
+    for i in 1..=n {
+        facsum *= i
+    }
+    return facsum
 }
 
 #[cfg(test)]
