a17b9b20d03016caad76923db5b85f7d9e4afa17
Ifeanyi batman <anyimifeanyi0@gmail.com>
2026-05-27 15:07:57 +0100

    completed chapter 5 exercises 1 - 11

--- exercises/05_ticket_v2/07_unwrap/src/lib.rs
+++ exercises/05_ticket_v2/07_unwrap/src/lib.rs
@@ -2,7 +2,20 @@
 //   When the description is invalid, instead, it should use a default description:
 //   "Description not provided".
 fn easy_ticket(title: String, description: String, status: Status) -> Ticket {
-    todo!()
+    match Ticket::new(title.clone(), description, status.clone()) {
+        Ok(ticket) => ticket,
+        Err(err) => {
+            if err == "Description cannot be empty" || err == "Description cannot be longer than 500 bytes" {
+                Ticket {
+                    title,
+                    description: "Description not provided".into(),
+                    status,
+                }
+            } else {
+                panic!("{}", err)
+            }
+        }
+    }
 }
 
 #[derive(Debug, PartialEq, Clone)]
--- exercises/05_ticket_v2/09_error_trait/src/lib.rs
+++ exercises/05_ticket_v2/09_error_trait/src/lib.rs
@@ -2,18 +2,49 @@
 //  When implementing `Display`, you may want to use the `write!` macro from Rust's standard library.
 //  The docs for the `std::fmt` module are a good place to start and look for examples:
 //  https://doc.rust-lang.org/std/fmt/index.html#write
+// 
+ use std::fmt::Display;
+ use std::fmt::Formatter;
+ use std::fmt::Error;
 
+ 
+#[derive(Debug)]
 enum TicketNewError {
     TitleError(String),
     DescriptionError(String),
 }
+
+impl Display for TicketNewError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result<(), Error> {
+        match self {
+        TicketNewError::TitleError(err) => write!(f, "{}",err),
+        TicketNewError::DescriptionError(err) => write!(f, "{}", err )
+        }
+    }
+}
+
+impl std::error::Error for TicketNewError {}
 
 // TODO: `easy_ticket` should panic when the title is invalid, using the error message
 //   stored inside the relevant variant of the `TicketNewError` enum.
 //   When the description is invalid, instead, it should use a default description:
 //   "Description not provided".
 fn easy_ticket(title: String, description: String, status: Status) -> Ticket {
-    todo!()
+    match Ticket::new(title.clone(), description, status.clone()) {
+        Ok(ticket) => ticket,
+        Err(err) => {
+            if let TicketNewError::TitleError 
+           (_) = err {
+                panic!("{}",err)
+            } else {
+                return Ticket {
+                    title,
+                    description: "Description not provided".to_string(),
+                    status
+                }
+            }
+        }
+    }
 }
 
 #[derive(Debug, PartialEq, Clone)]
